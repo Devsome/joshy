93fea62fd40a4a62d3ab0c09e03b7cf4d44fc4de
Bean <ihubnetceogeenomc@gmail.com>
2019-10-17 19:06:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-const token = 'insert-token-here';
+require("./keepOnline.js");
 
-bot.on('ready', () =>{
+const token = "insert-token-here";
+
+client.on('ready', () =>{
     console.log('Josh is now online! Type j! for help!');
 })
 
-bot.login(token);
+client.login(token);
 
 const prefix = "!";
 
-bot.on('message', msg=>{
+client.on('message', msg=>{
     if(msg.content ===  "!hi"){
         msg.reply('Hi Im Josh!');
     }
 
     if(msg.content.startsWith(prefix +"prune")){
     let args = msg.content.split(" ").slice(1);
-    let author = message.member;
-    let role = message.guild.role.find('name', "Doggo")
+    let author = msg.member;
+    let role = msg.guild.role.find('name', "Doggo")
     if(author.role.has(role.id)){
         if(!args){
-            message.delete();
-            message.channel.send("No arguments given");
+            msg.delete();
+            msg.channel.send("No arguments given");
             return;
         }
         if(args[0] > 100){
-            message.delete();
-            message.channel.send("Maxiumum is 100 messages at once.");
+            msg.delete();
+            msg.channel.send("Maxiumum is 100 messages at once.");
             return;
         }
 
-        message.delete();
-        message.channel.bulkDelete(args);
-        message.channel.send("Done! I deleted" + args[0] + " messages.");
+        msg.delete();
+        msg.channel.bulkDelete(args);
+        msg.channel.send("Done! I deleted" + args[0] + " messages.");
         return;
         }
     }
